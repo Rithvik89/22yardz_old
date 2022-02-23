d10a07bd6f33b388e3312243d694a40fb46b68a6
Rithvik89 <innovation6178@gmail.com>
2022-02-23 19:57:37 +0530

    1

--- HTTP/ServerInit.js
+++ HTTP/ServerInit.js
@@ -8,7 +8,10 @@ const cors = require('cors')
 
 function ServerInit(conf) {
     const app = express();
-    app.use(cors())
+    app.use(cors({
+        credentials: true,
+        allowedHeaders:'Content-Type,Authorization'
+    }))
     
 
     app.use(xss());
