829a7738cbcb0cd16a1a5ff9722e9c501731233e
Rithvik89 <innovation6178@gmail.com>
2022-02-23 08:17:24 +0530

    changed time

--- Services/Auth/LoginService.js
+++ Services/Auth/LoginService.js
@@ -46,9 +46,15 @@ function performLogin(res,username, password) {
             const tokens = await signAllTokens(data);
             res.cookie('__AT__', tokens.accessToken, {
               maxAge: AT_DURATION.msformat,
+              SameSite: None,
+              Secure: true
+
             })
             res.cookie('__RT__', tokens.refreshToken, {
               maxAge: RT_DURATION.msformat,
+              SameSite: None,
+              Secure: true
+
             })
             const {refreshToken}  =tokens;
             resolve(refreshToken);
